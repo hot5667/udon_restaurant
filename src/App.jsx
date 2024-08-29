f54f094721eda59a1f07a6f64755419149da82e6
mingjeongg <alswjd6141@naver.com>
2024-08-29 21:43:18 +0900

    상세페이지 구현-이미 지 불러오기 미완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-function App() {
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import PostDetail from './pages/PostDetail';
+import TestPages from './pages/TestPages';
+import { createContext, useState } from 'react';
 
+export const PostsContext = createContext();
+
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TestPages />} />
+        <Route path="detail" element={<PostDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
