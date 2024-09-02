088bac7d3d31c0e48e1934e265c31feac7aaaa42
mingjeongg <alswjd6141@naver.com>
2024-09-02 22:48:49 +0900

    ,

--- src/pages/post-page/PostDetail.jsx
+++ src/pages/post-page/PostDetail.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
+
 import { useSearchParams } from "react-router-dom";
 import supabase, { supabaseUrl } from "../../supaBasecClient";
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 const PostDetail = () => {
   const [searchParam] = useSearchParams();
