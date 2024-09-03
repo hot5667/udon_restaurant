13913c758170a04c4676a8cdb41a9dd5d78bfac1
mingjeongg <alswjd6141@naver.com>
2024-09-03 16:52:37 +0900

    완성

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,52 +1,57 @@
-import React, { useContext } from 'react'
-import styled from 'styled-components'
-import { PostContext } from '../context/PostContext';
-import { Navigate, useNavigate } from 'react-router-dom';
-import defaultImg from '../img/default-img.png'
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { PostContext } from "../context/PostContext";
+import { Navigate, useNavigate } from "react-router-dom";
+import defaultImg from "../img/default-img.png";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const STORAGE_NAME = 'images';
+const STORAGE_NAME = "images";
 
-const PostCard = ({post}) => {
-  const {PostID, PostImgs, PostTitle, PostLike} = post;
-  const {user} = useContext(PostContext);
+const PostCard = ({ post }) => {
+  const { PostID, PostImgs, PostTitle, PostLike } = post;
+  const { user } = useContext(PostContext);
   const navigate = useNavigate();
   let imgArray = JSON.parse(PostImgs);
-  if (imgArray.some(img => img.includes('https'))){
+  if (imgArray.some((img) => img.includes("https"))) {
     imgArray = [];
   }
 
   // console.log(PostID, imgArray);
   return (
-    <Card onClick={(e) => {
-      e.preventDefault();
-      if (user) {
-      alert(`Post ID : ${PostID}`);
-
-      }else {
-        alert('로그인해야 확인 가능합니다. 로그인 페이지로 이동합니다.');
-        navigate('/sign-in');
-        return;
-      }
-    }}>
-      
-      {
-      imgArray.length === 0 ?
-      <img src={defaultImg} />
-      : <img src={`${supabaseUrl}/storage/v1/object/public/${STORAGE_NAME}/${post.PostID}/${imgArray[0]}`} />
-      }
-      <p className='post_title'>{PostTitle}</p>
-      <div className='post_like_container'>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path fill="#ff0000" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
-      </svg>
-      <p className='post_like'>{PostLike}</p>
+    <Card
+      onClick={(e) => {
+        e.preventDefault();
+        if (user) {
+          navigate(`/detail?id=${PostID}`);
+        } else {
+          alert("로그인해야 확인 가능합니다. 로그인 페이지로 이동합니다.");
+          navigate("/sign-in");
+          return;
+        }
+      }}
+    >
+      {imgArray.length === 0 ? (
+        <img src={defaultImg} />
+      ) : (
+        <img
+          src={`${supabaseUrl}/storage/v1/object/public/${STORAGE_NAME}/${post.PostID}/${imgArray[0]}`}
+        />
+      )}
+      <p className="post_title">{PostTitle}</p>
+      <div className="post_like_container">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path
+            fill="#ff0000"
+            d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
+          />
+        </svg>
+        <p className="post_like">{PostLike}</p>
       </div>
     </Card>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
 
 const Card = styled.div`
   width: 255px;
@@ -84,7 +89,7 @@ const Card = styled.div`
     align-items: center;
     justify-content: flex-start;
   }
-  
+
   .post_like_container .post_like {
     /* margin: auto; */
   }
@@ -94,5 +99,4 @@ const Card = styled.div`
     margin-right: 10px;
     color: red;
   }
-`
-
+`;
--- src/context/PostContext.jsx
+++ src/context/PostContext.jsx
@@ -3,7 +3,7 @@ import supabase from "../supaBasecClient";
 
 export const PostContext = createContext(null);
 
-const STORAGE_NAME = 'images';
+const STORAGE_NAME = "images";
 
 const PostContextProvider = ({ children }) => {
   const [postsNumber, setPostsNumber] = useState(0);
@@ -16,9 +16,9 @@ const PostContextProvider = ({ children }) => {
       if (error) {
         throw error;
       } else {
-        data.sort((a, b) => a.PostID - b.PostID)
+        data.sort((a, b) => a.PostID - b.PostID);
         // console.log("data => ", data);
-        setPostsNumber(prev => prev + data.at(-1).PostID + 1);
+        setPostsNumber((prev) => prev + data.at(-1).PostID + 1);
       }
     };
 
@@ -32,7 +32,6 @@ const PostContextProvider = ({ children }) => {
     } else {
       console.log("add data succeed", post.PostID);
     }
-
   };
 
   // const deleteData = async (post) => {
@@ -45,19 +44,22 @@ const PostContextProvider = ({ children }) => {
   // }
 
   const modifyData = async (post) => {
-    console.log('modyfying :', post);
+    console.log("modyfying :", post);
     const { PostTitle, PostContent, PostCity, PostFoodType } = post;
-    const { error } = await supabase.from("Post").update({ PostTitle, PostContent, PostCity, PostFoodType }).eq('PostID', post.PostID);
+    const { error } = await supabase
+      .from("Post")
+      .update({ PostTitle, PostContent, PostCity, PostFoodType })
+      .eq("PostID", post.PostID);
     if (error) {
       throw error;
     } else {
       console.log("modify data succeed", post.PostID);
     }
-  }
+  };
 
   const addPost = (newPost) => {
     addData({ PostID: postsNumber, ...newPost });
-  }
+  };
 
   // const deletePost = (curPost) => {
   //   deleteData(curPost);
@@ -66,110 +68,116 @@ const PostContextProvider = ({ children }) => {
   const modifyPost = (newPost) => {
     modifyData(newPost);
     // deleteImgs(newPost.PostID);
-  }
-
+  };
 
   const uploadImgs = async (PostID, files) => {
     for (let i = 0, n = files.length; i < n; i++) {
-      const { data, error } = await supabase
-        .storage
+      const { data, error } = await supabase.storage
         .from(STORAGE_NAME)
-        .upload(`${PostID}/${PostID}_${i}`, files[i])
+        .upload(`${PostID}/${PostID}_${i}`, files[i]);
       // 저장시 이름을 순서에 맞춰, 사진 중복 업로드도 허용함.
       if (error) {
         throw error;
       }
     }
-
-  }
+  };
 
   const getImgs = async (PostID) => {
-    const { data, error } = await supabase
-      .storage
+    const { data, error } = await supabase.storage
       .from(STORAGE_NAME)
-      .list(`${PostID}`)
+      .list(`${PostID}`);
     if (error) {
       throw error;
     } else {
-      console.log('27 :', data)
+      console.log("27 :", data);
       // setData(data);
     }
-  }
+  };
 
   const deleteImgs = async (PostID) => {
-    let { data: curData, error: getError } = await supabase
-      .storage
+    let { data: curData, error: getError } = await supabase.storage
       .from(STORAGE_NAME)
-      .list(`${PostID}`)
+      .list(`${PostID}`);
     if (getError) {
       throw error;
     }
     // else {
-    console.log('curData :', curData)
-    curData = curData.map(ele => PostID + '/' + ele.name);
+    console.log("curData :", curData);
+    curData = curData.map((ele) => PostID + "/" + ele.name);
 
     // }
 
-    const { data, error: deleteError } = await supabase
-      .storage
+    const { data, error: deleteError } = await supabase.storage
       .from(STORAGE_NAME)
-      .remove(curData)
+      .remove(curData);
     if (deleteError) {
       throw deleteError;
     } else {
-      console.log('delete :', data)
+      console.log("delete :", data);
       // setData(data);
     }
-  }
+  };
 
   // const STORAGE_NAME = 'images';
   const uploadFile = async (PostID, file) => {
-    const { error } = await supabase
-      .storage
+    const { error } = await supabase.storage
       .from(STORAGE_NAME)
-      .upload(`${PostID}/${file.name}`, file)
+      .upload(`${PostID}/${file.name}`, file);
     if (error) {
       throw error;
     }
-  }
-  
+  };
 
   useEffect(() => {
     const getUserInfo = async () => {
-      const { data:userData } = await supabase.auth.getSession();
+      const { data: userData } = await supabase.auth.getSession();
       if (userData.session) {
         // console.log(userData);
 
-        const { data, error } = await supabase.from('User').select('UserNickName').eq('UserID', userData.session.user.id);
+        const { data, error } = await supabase
+          .from("User")
+          .select("UserNickName")
+          .eq("UserID", userData.session.user.id);
         // console.log(data);
-        setUser({UserID:userData.session.user.id, UserNickname:data[0].UserNickName})
+        setUser({
+          UserID: userData.session.user.id,
+          UserNickname: data[0].UserNickName,
+        });
       } else {
         setUser(null);
       }
-    }
+    };
 
     getUserInfo();
-  },[])
+  }, []);
 
-  
   const signOutUser = async () => {
     const { error } = await supabase.auth.signOut();
 
     if (error) {
-      console.error('로그아웃 중 오류가 발생했습니다:', error.message);
+      console.error("로그아웃 중 오류가 발생했습니다:", error.message);
     } else {
       // 로그아웃 성공 시 로그인 페이지로 리디렉션
       // navigate('/sign-in');
       setUser(null);
     }
   };
-  
 
   return (
-    <PostContext.Provider value={{ setPostsNumber, addPost, modifyPost, uploadImgs, deleteImgs, signOutUser, user }}>
+    <PostContext.Provider
+      value={{
+        setPostsNumber,
+        addPost,
+        modifyPost,
+        uploadImgs,
+        deleteImgs,
+        signOutUser,
+        user,
+      }}
+    >
       {children}
     </PostContext.Provider>
-  )
-}
+  );
+};
 
 export default PostContextProvider;
